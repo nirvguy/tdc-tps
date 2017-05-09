3d0b28fe89e4ae39de003c35ca66b00d3c260727
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-09 18:30:27 -0300

    Le agrega parametros al programa para el timeout y la interfaz

--- tp1/src/ej1.py
+++ tp1/src/ej1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import argparse
 from scapy.all import *
 from fuente import Fuente
 
 UNICAST = 'unicast'
 MULTICAST = 'multicast'
+TIMEOUT_DEFAULT = 60 * 10
 
 fuente = Fuente(simbolos=(UNICAST, MULTICAST))
 
@@ -14,9 +16,17 @@ def packet_callback(packet):
         fuente.agregar_muestra(UNICAST)
 
 def main():
-    sniff(iface=config.EJ1_IFACE, prn=packet_callback, store=0, timeout=60)
+    parser = argparse.ArgumentParser(description='Modelado de fuente unicast/broadcast')
+    parser.add_argument('-t', "--timeout", type=int, default=TIMEOUT_DEFAULT, help='Tiempo de sniffeo')
+    parser.add_argument('-i', "--iface", type=str, help='Interface de sniffeo')
+    args = parser.parse_args()
+    if args.iface:
+        sniff(iface=args.iface, prn=packet_callback, store=0, timeout=args.timeout)
+    else:
+        sniff(prn=packet_callback, store=0, timeout=args.timeout)
     print(fuente)
     print("Entropia : {}".format(fuente.entropia()))
+
 
 if __name__ == '__main__':
     main()
