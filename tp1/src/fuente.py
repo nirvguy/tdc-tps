829c0401aa3dbef02677de12ec57a5f352a8a504
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-09 13:15:00 -0300

    Agrega clase para modelar las fuentes de informacion

---
+++ tp1/src/fuente.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import math
+
+class Fuente:
+    def __init__(self, simbolos):
+        self.muestras = {s: 0 for s in simbolos}
+        self.cantidad_muestras = 0
+    
+    def __str__(self):
+        return "\n".join("{}: {}".format(s, p) for s, p in self.probabilidades())
+
+    def probabilidades(self):
+        if self.cantidad_muestras == 0 or len(self.muestras) == 0:
+            raise Exception("No se registraron muestras en la fuente")
+        return {(s, c / self.cantidad_muestras) for s, c in self.muestras.items()}
+
+    def agregar_muestra(self, simbolo):
+        self.cantidad_muestras += 1
+        c = self.muestras.get(simbolo) or 0
+        self.muestras[simbolo] = c + 1
