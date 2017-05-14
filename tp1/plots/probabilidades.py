4e0fedcea51cf8f5b77d27a3b464a5fc50c77790
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-14 19:54:43 -0300

    Agrega en el plot en nodo destacado

--- tp1/plots/probabilidades.py
+++ tp1/plots/probabilidades.py
@@ -5,11 +5,21 @@ import json
 import matplotlib.pyplot as plt
 
 def plot(probabilidades):
+    def destacados():
+        max_prob = None
+        for _, p in probabilidades.items():
+            if max_prob is None:
+                max_prob = p
+            elif max_prob < p:
+                max_prob = p
+        return [0.1 if p == max_prob else 0.0 for _, p in probabilidades.items()]
+
+    explode = destacados()
     labels = ["{} ({:3.2f}%)".format(s, p * 100.0) for s, p in probabilidades.items()]
     sizes = [ i * 100 for i in probabilidades.values() ]
     fig, ax = plt.subplots(figsize=(10, 10))
-    patches = ax.pie(sizes, autopct='%1.1f%%', startangle=90, shadow=True)
     ax.legend(labels=labels, loc="best")
+    patches = ax.pie(sizes, autopct='%1.1f%%', startangle=90, shadow=True, explode=explode)
     # plt.axis('equal')
     fig.tight_layout()
     fig.savefig(sys.stdout.buffer)
