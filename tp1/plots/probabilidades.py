18d2f69b08e6877d93423cdf91f82bf8f1d21faa
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-15 11:23:49 -0300

    Agrega tabla de entropias y restructura las imagenes en el informe

--- tp1/plots/probabilidades.py
+++ tp1/plots/probabilidades.py
@@ -4,6 +4,7 @@ import sys
 import json
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib import font_manager as fm
 import numpy as np
 import operator
 import random
@@ -23,8 +24,12 @@ def plot(probabilidades):
     labels = ["{} ({:3.2f}%)".format(s, p * 100.0) for s, p in probabilidades]
     sizes = [ p * 100 for _, p in probabilidades ]
     fig, ax = plt.subplots(figsize=(10, 10))
-    patches = ax.pie(sizes, autopct='%1.1f%%', startangle=90, shadow=True, explode=explode, colors=colors)
-    ax.legend(labels=labels, loc="best")
+    patches,texts,autotexts = ax.pie(sizes, autopct='%1.1f%%', startangle=90, shadow=True, explode=explode, colors=colors)
+    proptease = fm.FontProperties()
+    proptease.set_size(14)
+    plt.setp(autotexts, fontproperties=proptease)
+    plt.setp(texts, fontproperties=proptease)
+    ax.legend(labels=labels, loc="best", fontsize=12)
     # plt.axis('equal')
     fig.tight_layout()
     fig.savefig(sys.stdout.buffer)
