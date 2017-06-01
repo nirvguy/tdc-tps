e7ab3f9c23c82dbe6516cc4b3e1d990612ccf3f4
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-01 00:49:32 -0300

    Actualiza script de grafico. Juanta los dos en uno

--- tp2/plot/rtts.py
+++ tp2/plot/rtts.py
@@ -4,36 +4,59 @@ import sys
 import json
 import matplotlib.pyplot as plt
 import operator
+import numpy as np
 import math
 
-def plot(trace):
-    fig, ax = plt.subplots()
-    n = len(trace)
-    ind = range(n)
-    ind_labels = range(n+1)
+def plot(trace, value_table):
+    length = len(trace)
+    ind_labels = range(length)
     labels = []
     for i in ind_labels:
-        if i == 0:
-            labels.append("")
-        else:
-            labels.append(trace[i-1]['ip'])
-    # for i in ind:
-    #     if i == 0:
-    #         labels.append(trace[i]['ip'])
-    #     else:
-    #         labels.append("{} - {}".format(trace[i-1]['ip'],trace[i]['ip']))
-    rtts = list(map(lambda x: x['rtt'] * 1000, trace))
-    bar_width = 0.3
-    ax.bar(ind, rtts, bar_width)
-    ax.set_xticks(list(map(lambda x:x-0.5, ind_labels)))
-    ax.set_ylabel('RTT enlace (ms)')
-    ax.set_xticklabels(labels, rotation=90)
-    fig.tight_layout()
-    fig.savefig(sys.stdout.buffer)
+        labels.append(trace[i-1]['ip'])
+
+    plt.rc('text', usetex=True)
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    # Carga datos apartir de trace
+    mean_rtts = list(map(lambda x: x['mean_total_rtt'] * 1000, trace))
+    std_rtts = list(map(lambda x: x['std_total_rtt'] * 1000,trace))
+    norm_rtts = list(map(operator.itemgetter('norm_rtt'),trace))
+    colors = np.array(['teal' if n < value_table else 'orange' for n in norm_rtts])
+
+    ##############################################
+    ##                                          ##
+    ##  Barplot con los delta RTTS normalizados ##
+    ##                                          ##
+    ##############################################
+    bar_width = 0.2
+    # Eje X
+    ax1.set_xticks(ind_labels)
+    ax1.set_xlabel('IPS', fontsize=20)
+    ax1.set_xticklabels(labels, rotation=90)
+    # Eje Y
+    ax1.set_ylabel('$\\frac{\\Delta RTT-\\mu}{\\sigma}$', color='teal', fontsize=20)
+    ax1.bar(range(length), norm_rtts, bar_width, color=colors)
+    # Linea de corte
+    ax1.axhline(y=value_table, color='orange')
+    ax1.text(0.5, value_table + 0.1, 'Nivel de corte', color='orange', fontsize=20)
+
+    ##############################################
+    ##                                          ##
+    ##     Grafico con los RTTs totales         ##
+    ##                                          ##
+    ##############################################
+    ax2.errorbar(range(length), mean_rtts, std_rtts, marker='.', color='red')
+    ax2.grid(color='gray', linewidth=1)
+    ax2.set_ylabel('Total RTT (ms)', color='r', fontsize=20)
+
+    plt.tight_layout()
+    plt.show()
 
 def main():
     if len(sys.argv) < 2:
-        print("Se esperaba el archivo json de entrada", file=sys.stderr)
+        # print("Se esperaba el archivo json de entrada", file=sys.stderr)
         sys.exit(1)
 
     filename = sys.argv[1]
@@ -44,7 +67,7 @@ def main():
         if 'trace' not in data:
             raise Exception("Archivo json invalido!")
 
-        plot(data['trace'])
+        plot(data['trace'], data['value_table'])
 
 if __name__ == '__main__':
     main()
